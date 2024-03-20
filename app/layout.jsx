f83986530d97a01e5b63840d3970775de7ba3357
Deadlymind <deadlygamex5@gmail.com>
2024-03-20 23:57:06 +0100

    create Header , Footer , ThemeProvider and ThemeToggler

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,13 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+// components
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +18,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} supperssHydrationWarning >
+        <ThemeProvider attribute='class' defaulttheme='light' >
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
