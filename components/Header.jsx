678849ddfa369c35f1459858c742d21416bdc609
Deadlymind <deadlygamex5@gmail.com>
2024-03-21 07:18:47 +0100

    create hero and update nav/header

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,10 +6,27 @@ import Logo from "./Logo";
 import ThemeToggler from "./ThemeToggler";
 import Nav from "./Nav";
 import MobileNav from "./MobileNav";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 const Header = () => {
+    const [header, setHeader] = React.useState(false);
+    const pathname = usePathname();
+
+    useEffect(() => {
+        const scrollYPos = window.addEventListener('scroll', () => {
+            window.scrollY > 50 ? setHeader(true) : setHeader(false);
+        });
+
+        // remove event
+        return () => window.removeEventListener('scroll', scrollYPos);
+    });
     return (
-        <header>
+        <header className={`${
+            header
+            ? 'py-4 bg-white shadow-lg dark:bg-accent '
+            : 'py-6 dark:bg-transparent '
+            } sticky top-0 z-30 transition-all ${pathname === '/' && 'bg-[#fef9f5]' } `} >
             <div className="container mx-auto">
                 <div className="flex justify-between items-center">
                     <Logo />
