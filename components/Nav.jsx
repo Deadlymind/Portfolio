7dc995b749fe2540163810fa7e187a70478d66cb
Deadlymind <deadlygamex5@gmail.com>
2024-03-21 06:22:16 +0100

    update nav , header and create projects page

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,32 @@
+// link (next js)
+import Link from 'next/link';
 
-const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
+// next hooks
+import { usePathname } from 'next/navigation';
+
+const link = [
+    {path: "/", name: "home"},
+    {path: "/projects", name: "my projects"},
+    {path: "/contact", name: "contact"},
+]
+
+
+const Nav = ({ containerStyles, linkStyles, underlineStyles }) => {
+    const path = usePathname();
+    return (
+    <nav className={`${containerStyles}`} >
+        {link.map((link, index) => {
+            return (
+                <Link
+                    href={link.path}
+                    key={index}
+                    className={`capitalize ${linkStyles}`}
+                >
+                    {link.name}
+                </Link> );
+        } )}
+    </nav>
+    )
 }
 
 export default Nav
