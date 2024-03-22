48bf5e14ea1444c3fd9b66b6a61403f6d6d2f6b7
Deadlymind <deadlygamex5@gmail.com>
2024-03-22 17:40:35 +0100

    update Hero , Globalcss and Themetoggler

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,20 +10,40 @@ import {
 } from 'react-icons/ri';
 
 // components
-import Devlmg from './Devlmg';
+import DevImg from './DevImg';
 import Badge from './Badge';
 import Socials from './Socials';
 
 
 const Hero = () => {
     return (
-    <section className='py-12 xl:py-24 h-[84vh] xl:pt-28 bg-pink-100 ' >
+    <section className='py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none' >
         <div className="container mx-auto">
             <div className='flex justify-between gap-x-8' >
                 {/* text */}
-                <div>text</div>
+                <div className='flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left ' >
+                    <div className='text-sm uppercase font-semibold mb-4 text-primary tracking-[4px] ' >Software developer</div>
+                    <h1 className='h1 mb-4' >Hello, my in name is Oussama Ayari</h1>
+                    <p className='subtitle max-w-[490px] mx-auto xl:mx-0 ' >
+                        Brief description with insight into myself,
+                        my vocational journey,
+                        and what i engage in professionally.
+                    </p>
+                    {/* buttons */}
+                    <div className='flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12 ' >
+                        <Link href="/contact" >
+                            <Button className='gap-x-2' >
+                                Contact me <Send size={18} />
+                            </Button>
+                        </Link>
+                        <Button variant='secondary' className='gap-x-2' >
+                                Download cv
+                                <Download size={18} />
+                        </Button>
+                    </div>
+                </div>
                 {/* image */}
-                <div>image</div>
+                <div className='hidden xl:flex relative ' >image</div>
             </div>
             {/* icon */}
             <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce '  >
