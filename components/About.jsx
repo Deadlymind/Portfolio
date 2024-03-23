60a9143230755e638e22f9be08c29a699c389805
Deadlymind <deadlygamex5@gmail.com>
2024-03-23 10:28:04 +0100

    update about , Globalcss and tailwind

--- components/About.jsx
+++ components/About.jsx
@@ -39,33 +39,38 @@ const qualificationsData = [
         data: [
             {
                 university: 'online harvard university',
-                qualification: 'Computer Science',
-                year: '2018 - 2019',
+                qualification: 'CS50',
+                years: '2018 - 2019',
             },
             {
                 university: 'Coursera',
                 qualification: 'Meta Back-End Developer',
-                year: '2023 - 2024',
+                years: '2023 - 2024',
             },
             {
                 university: 'CODE4LEARN',
                 qualification: 'fULL STACK WEB DEVELOPER',
-                year: '2023 - 2024',
+                years: '2023 - 2024',
             },
             {
                 university: 'CODEZILLA',
                 qualification: 'MASTER PYTHON DEVELOPER',
-                year: '2023 - 2024',
+                years: '2023 - 2024',
             },
         ],
     },
     {
-        title: 'Experience',
+        title: 'experience',
         data: [
             {
                 company: 'Potato Academy',
-                qualification: 'Founder and CEO and Developer',
-                year: '2024 - present',
+                role: 'Founder, SEO and Developer',
+                years: '2024 - present',
+            },
+            {
+                company: 'FreeLancer',
+                role: 'Software Developer',
+                years: '2023 - present',
             },
         ]
     }
@@ -201,10 +206,77 @@ const About = () => {
                                             );
                                         })}
                                     </div>
+                                    {/* languages */}
+                                    <div className="flex flex-col gap-y-2 " >
+                                        <div className="text-primary" >Languages</div>
+                                        <div className="border-b border-border"></div>
+                                        <div>Arabic, English</div>
+                                    </div>
                                 </div>
                             </TabsContent>
                             {/* qualifications */}
-                            <TabsContent value='qualifications'>Qualifications</TabsContent>
+                            <TabsContent value='qualifications'>
+                                <div>
+                                    <h3 className="h3 mb-8 text-center xl:text-left " >My Awesome Journey</h3>
+                                    {/* experience & education wrapper */}
+                                    <div className="grid md:grid-cols-2 gap-y-8 " >
+                                        {/* experience */}
+                                        <div className="flex flex-col gap-y-6 ml-2 " >
+                                            <div className="flex items-center gap-x-4 text-[22px] text-primary " >
+                                                <Briefcase />
+                                                <h4 className="capitalize font-medium" >
+                                                    {getData(qualificationsData,'experience').title}
+                                                </h4>
+                                            </div>
+                                            {/* list*/}
+                                            <div className="flex flex-col gap-y-8" >
+                                                {getData(qualificationsData,'experience').data.map((item, index) => {
+                                                    const {company, role, years} = item;
+                                                    return (
+                                                        <div className="flex gap-x-8 group " key={index} >
+                                                            <div className="h-[84px] w-[1px] bg-border relative ml-2 " >
+                                                                <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500 " ></div>
+                                                            </div>
+                                                            <div>
+                                                                <div className="font-semibold text-xl leading-none mb-2  " >{company}</div>
+                                                                <div className="text-lg leading-none text-muted-foreground mb-4 " >{role}</div>
+                                                                <div className="text-base font-medium" >{years}</div>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        </div>
+                                        {/* education */}
+                                        <div className="flex flex-col gap-y-6 ml-2 " >
+                                            <div className="flex items-center gap-x-4 text-[22px] text-primary " >
+                                                <GraduationCap size={28} />
+                                                <h4 className="capitalize font-medium" >
+                                                    {getData(qualificationsData,'education').title}
+                                                </h4>
+                                            </div>
+                                            {/* list*/}
+                                            <div className="flex flex-col gap-y-8" >
+                                                {getData(qualificationsData,'education').data.map((item, index) => {
+                                                    const {university, qualification, years} = item;
+                                                    return (
+                                                        <div className="flex gap-x-8 group " key={index} >
+                                                            <div className="h-[84px] w-[1px] bg-border relative ml-2 " >
+                                                                <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500 " ></div>
+                                                            </div>
+                                                            <div>
+                                                                <div className="font-semibold text-xl leading-none mb-2  " >{university}</div>
+                                                                <div className="text-lg leading-none text-muted-foreground mb-4 " >{qualification}</div>
+                                                                <div className="text-base font-medium" >{years}</div>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </TabsContent>
                             {/* skills */}
                             <TabsContent value='skills'>Skills</TabsContent>
                         </div>
