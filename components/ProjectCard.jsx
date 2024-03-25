e53c7be7e894be1f322efb3f42418f9526484df1
Deadlymind <deadlygamex5@gmail.com>
2024-03-25 08:07:10 +0100

    update project card

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -4,11 +4,35 @@ import { Card, CardHeader } from "@/components/ui/card";
 import { Github, Link2Icon } from "lucide-react";
 import { Badge } from './ui/badge';
 
-const ProjectCard = ({project}) => {
+const ProjectCard = ({ project }) => {
   console.log(project);
   return (
-    <Card>ProjectCard</Card>
-  )
-}
+    <Card className='w-full h-full group overflow-hidden relative ' >
+        <CardHeader className='p-0   ' >
+          {/* image */}
+          <div className='relative w-full h-[300px] flex items-center justify-center bg-tertiary dark:bg-secondary/40 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden  ' >
+            <Image
+            className='absolute bottom-0 shadow-2xl '
+            src={project.image}
+            width={247}
+            height={250}
+            alt=''
+            priority
+            />
+            {/* buttons links */}
+
+            <div>
+              <Link href={project.link} ><Link2Icon/> </Link>
+            </div>
+          </div>
+        </CardHeader>
+        <div className='h-full px-8 py-6  ' >
+          <Badge className='uppercase text-sm font-medium mb-2 absolute top-4 left-5'>{project.category}</Badge>
+          <h4 className='h4 mb-2' >{project.name}</h4>
+          <p className='text-muted-foreground text-lg ' >{project.description}</p>
+        </div>
+    </Card>
+  );
+};
 
 export default ProjectCard
--- components/Work.jsx
+++ components/Work.jsx
@@ -79,7 +79,7 @@ const Work = () => {
     <section className="relative mb-12 xl:mb-48 " >
         <div className="container mx-auto ">
             {/* text */}
-            <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start" >
+            <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left  mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start " >
                 <h2 className="section-title mb-4 " >Latest Projects</h2>
                 <p className="subtitle mb-8 " >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, dolorem.</p>
                 <Link href="/projects" ><Button>All projects</Button></Link>
@@ -87,30 +87,30 @@ const Work = () => {
             {/* slider */}
             <div className="xl:max-w-[1000px] xl:absolute right-0 top-0  " >
                 <Swiper
-                    className='h-[400px]'
+                    className='h-[480px] '
                     slidesPreviews={1}
                     breakpoints={{
                         640: {
                             slidesPerView: 2,
-                        }
+                        },
                     }}
                     spaceBetween={30}
                     modules={[Pagination]}
                     pagination={{ clickable: true }}
                 >
                     {/* show only the first 4 projects for the slides */}
-                    {projectData.slice(0,5).map((project, index) => {
+                    {projectData.slice(0, 4).map((project, index) => {
                         return (
                             <SwiperSlide key={index}>
                                 <ProjectCard project={project} />
                             </SwiperSlide>
-                        )
+                        );
                     })}
                 </Swiper>
             </div>
         </div>
     </section>
-    )
+    );
 }
 
 
